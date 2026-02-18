8049dd0c399d05d9da5492c030547eb580a030ef
Satyam Gupta <sg8440234@gmail.com>
2026-02-19 00:16:56 +0530

    fixed axios config

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   
-  baseURL: "http://localhost:8008",
+  baseURL: "",
   headers: {
     "Content-Type": "application/json",
   },
